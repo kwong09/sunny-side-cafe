b3982a51226824c724552b3b4c15f071aff12089
kwong09 <wongkayla09@gmail.com>
2025-06-25 07:05:15 +0000

    order screen layout finished

---
+++ script.js
@@ -0,0 +1,12 @@
+let homeScreen = document.querySelector(".homeScreen");
+let startButton = document.getElementById("startButton");
+
+let orderScreen = document.querySelector(".orderScreen");
+
+function visible(obj) {
+    obj.style.display = "block";
+}
+
+function invisible(obj) {
+    obj.style.display = "none";
+}
